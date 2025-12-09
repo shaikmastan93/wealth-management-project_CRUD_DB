889ab961f9162473876783141b54025ebb34ff58
Rukmini T <rukminitiragamalla1@gmail.com>
2025-12-09 18:37:40 +0530

    Added /hi API in main.py

--- main.py
+++ main.py
@@ -43,6 +43,9 @@ class UserUpdate(BaseModel):
 def hello():
     return {"message": "FastAPI + DB connected!"}
 
+@app.get("/hi")
+def hi():
+    return {"message":"hi connected with fastapi"}
 
 # ---------------------------
 # CREATE USER (C)
