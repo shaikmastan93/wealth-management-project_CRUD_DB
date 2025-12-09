10e54ab47640d5cd3c421e89a57fa23c4cd2b998
bhavyashukla510-cloud <bhavyashukla510@gmail.com>
2025-12-09 19:37:53 +0530

    the api is working

--- database.py
+++ database.py
@@ -3,7 +3,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Welcome%40123@localhost:5432/wealth_db"
+DATABASE_URL = "postgresql://postgres:postgres2210@localhost:5432/wealth_db"
 
 
 engine = create_engine(DATABASE_URL)
